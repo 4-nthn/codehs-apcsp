ee61871852c36f9ba8d4c51ed7757f3abef22829
nathan <40584011+4-nthn@users.noreply.github.com>
2024-09-10 07:15:39 -0400

    Create 1.7.4: The Two Towers + Comments.py

---
+++ Unit 1: Introduction to Programming/1.7: Commenting your Code/1.7.4: The Two Towers + Comments.py
@@ -0,0 +1,28 @@
+# carol building a tower of three balls
+
+def place_stack():
+    put_ball()
+    turn_left()
+    move()
+    put_ball()
+    move()
+    put_ball()
+
+# learning how to turn right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+place_stack()
+turn_left()
+turn_left()
+move()
+move()
+turn_left()
+move()
+move()
+place_stack()
+move()
+turn_right()
